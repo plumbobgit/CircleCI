29d8f6c0a41b0287d859a4cf7b00662e375bc7fa
plumbobgit <plumbob@oregonstate.edu>
2021-06-05 14:14:35 -0700

    One Commit based off Lecture

---
+++ README.md
@@ -0,0 +1,1 @@
+This is heavily based off the implementation we were shown in lecture because I was a little confused as to what exactly the assignment was asking.
---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+import calculator
+
+
+class TestCalculator:
+
+    def test_add(self):
+        assert calculator.add(2, 2) == 4
+
+    def test_subtract(self):
+        assert calculator.subtract(2, 2) == 0
+
+    def test_multiply(self):
+        assert calculator.multiply(2, 2) == 4
